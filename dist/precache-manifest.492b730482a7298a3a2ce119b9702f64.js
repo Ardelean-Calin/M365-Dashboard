e68cc50d59b0f997739a0dd17a1b5be0d31b130b
Ardelean Calin <ardelean.calin@outlook.com>
2019-02-24 16:43:45 +0200

    Uploaded build of alpha version.

---
+++ dist/precache-manifest.492b730482a7298a3a2ce119b9702f64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6073e48f0838c837122d",
+    "url": "/css/app.e88bd3fc.css"
+  },
+  {
+    "revision": "6073e48f0838c837122d",
+    "url": "/js/app.0a7598a1.js"
+  },
+  {
+    "revision": "a69f02c269e5cc392f45",
+    "url": "/js/chunk-vendors.9b60649b.js"
+  },
+  {
+    "revision": "72233eb1ed3ea1d647b29b672dcf364e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
